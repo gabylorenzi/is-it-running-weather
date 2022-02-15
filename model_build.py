a53dbb17e9eea0744b2e88ed2f511a2b3bfdb5a6
Gabrielle Lorenzi <gabylorenzi@Gabrielles-MBP.attlocal.net>
2022-02-15 10:09:59 -0800

    improved model and more training data

--- model_build.py
+++ model_build.py
@@ -2,29 +2,11 @@ import sklearn
 import pandas as pd 
 
 df = pd.read_csv('activities.csv')
-full_features_list = ['Weather Condition',
-'Weather Temperature', #temp
-'Apparent Temperature', #feels_like
-'Dewpoint', 
-'Humidity', #humidity
-'Wind Speed', #speed
-'Wind Gust', #gust
-'Wind Bearing', 
-'Precipitation',  
-'Intensity', 
-'Precipitation Probability', 
-'Precipitation Type', 
-'Cloud Cover',  #clouds
-'Weather Visibility', 
-'UV Index', 
-'Weather Ozone'
-]
 
 matched_features_list = ['Weather Temperature', #temp
 'Apparent Temperature', #feels_like
 'Humidity', #humidity
 'Wind Speed', #speed
-#'Wind Gust', #gust
 'Cloud Cover'  #clouds
 ]
 
@@ -38,12 +20,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 train_features, test_features, train_labels, test_labels = train_test_split(model_df[matched_features_list], model_df[target], test_size = 0.25, random_state = 42)
+
+cv_grid = {'n_estimators': [50,100,200],
+            'max_depth': [10,20,30],
+            'min_samples_split': [2,5],
+            'min_samples_leaf': [1,2,4]
+        }
+
 rf = RandomForestClassifier()
-rf.fit(train_features, train_labels)
 
-#OR send rf to app.py
+from sklearn.model_selection import GridSearchCV
+cv_rf = GridSearchCV(estimator = rf, param_grid = cv_grid, cv = 3, n_jobs = 1)
+
+cv_rf.fit(train_features, train_labels.values.ravel())
+
 def get_fit_model():
-    return rf
-
-predictions = rf.predict(test_features)
+    return cv_rf
 
